ffac30db817edc8840c62b263162c79735b0130c
Otemaah <124842414+Otemaah@users.noreply.github.com>
2023-04-19 21:41:25 -0400

    Add files via upload

---
+++ Q1.py
@@ -0,0 +1,6 @@
+for i in range(10):
+    num = int(input("Enter a number"))
+    if num % 2 ==0:
+        print("Even")
+    else:
+        print("odd")
---
+++ Q2.py
@@ -0,0 +1,3 @@
+import random as rand
+for x in range(1000):
+    print(rand.randint(1,100))
